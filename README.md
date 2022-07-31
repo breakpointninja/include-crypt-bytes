050dc2bdf137ba98672fa92327b524df0210e35c
BreakpointNinja <ninja@breakpoint.ninja>
2022-07-31 19:11:52 +0530

    feat(docs): Basic documentation and readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # include-crypt-bytes
-Rust macro to embed encrypted files in compiled binary
+
+Procedural macro to embed encrypted binary data in compiled binaries.
+
+The macro works similarly to [`std::include_str`], but instead of embedding the UTF-8
+string, it embeds encrypted `[u8]` array. Encryption uses the [chacha20poly1305][1] crate.
+
+## Examples
+
+```rust
+// CONFIG_PASSWORD is the environment variable set to the
+// string used to encrypt the contents of the file config.toml
+// at compile time
+//
+// `password` is the variable used to decrypt the embedded data
+// at runtime.
+let file_bytes = include_bytes_crypt!(
+                    "config.toml",
+                    password.as_bytes(),
+                    "CONFIG_PASSWORD");
+```
+
+Sometimes, its useful just to obfuscate the embedded data. In that case just the file name is required
+
+```rust
+// A random password is generated at compiled time
+let file_bytes = include_bytes_obfuscate!("config.toml");
+```
+
+[1]: https://docs.rs/chacha20poly1305
+
+License: MIT OR Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,33 @@
-pub use include_crypt_bytes_cipher::{decrypt_bytes, encrypt_bytes};
+//! Procedural macro to embed encrypted binary data in compiled binaries.
+//!
+//! The macro works similarly to [`std::include_str`], but instead of embedding the UTF-8
+//! string, it embeds encrypted `[u8]` array. Encryption uses the [chacha20poly1305][1] crate.
+//!
+//! # Examples
+//!
+//! ```rust
+//! # use include_crypt_bytes::include_bytes_crypt;
+//! # let password = "The password to use at runtime to decrypt";
+//! // CONFIG_PASSWORD is the environment variable set to the
+//! // string used to encrypt the contents of the file config.toml
+//! // at compile time
+//! //
+//! // `password` is the variable used to decrypt the embedded data
+//! // at runtime.
+//! let file_bytes = include_bytes_crypt!(
+//!                     "config.toml",
+//!                     password.as_bytes(),
+//!                     "CONFIG_PASSWORD");
+//! ```
+//!
+//! Sometimes, its useful just to obfuscate the embedded data. In that case just the file name is required
+//!
+//! ```rust
+//! # use include_crypt_bytes::include_bytes_obfuscate;
+//! // A random password is generated at compiled time
+//! let file_bytes = include_bytes_obfuscate!("config.toml");
+//! ```
+//!
+//! [1]: https://docs.rs/chacha20poly1305
+pub use include_crypt_bytes_cipher::{decrypt_bytes, encrypt_bytes, CryptError};
 pub use include_crypt_bytes_macro::{include_bytes_crypt, include_bytes_obfuscate};
